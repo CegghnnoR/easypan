dff91f29eb5848239de350c4e41fd0831e14cb35
CegghnnoR <1564011300@qq.com>
2024-07-11 22:01:52 +0800

    登录注册

---
+++ easypan/src/main/java/com/easypan/controller/FileInfoController.java
@@ -0,0 +1,42 @@
+package com.easypan.controller;
+
+import com.easypan.annotation.GlobalInterceptor;
+import com.easypan.entity.enums.FileCategoryEnums;
+import com.easypan.entity.enums.FileDelFlagEnums;
+import com.easypan.entity.query.FileInfoQuery;
+import com.easypan.entity.vo.PaginationResultVO;
+import com.easypan.entity.vo.ResponseVO;
+import com.easypan.service.FileInfoService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpSession;
+
+/**
+ * 文件信息 Controller
+ */
+@RestController("fileInfoController")
+@RequestMapping("file")
+public class FileInfoController extends ABaseController{
+
+	@Resource
+	private FileInfoService fileInfoService;
+	/**
+	 * 根据条件分页查询
+	 */
+	@RequestMapping("/loadDataList")
+	@GlobalInterceptor
+	public ResponseVO loadDataList(HttpSession session, FileInfoQuery query, String category) {
+		FileCategoryEnums categoryEnum = FileCategoryEnums.getByCode(category);
+		if (categoryEnum != null) {
+			query.setFileCategory(categoryEnum.getCategory());
+		}
+		query.setUserId(getUserInfoFromSession(session).getUserId());
+		query.setOrderBy("last_update_time desc");
+		query.setDelFlag(FileDelFlagEnums.USING.getFlag());
+		PaginationResultVO result = fileInfoService.findListByPage(query);
+		return getSuccessResponseVO(result);
+	}
+
+}
